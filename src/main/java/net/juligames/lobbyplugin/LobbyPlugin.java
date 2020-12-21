ce349a6808b5468f0a5040b76cee396e8630dc9a
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-12-21 11:23:47 +0100

    Fixed a Number Mistake

--- src/main/java/net/juligames/lobbyplugin/LobbyPlugin.java
+++ src/main/java/net/juligames/lobbyplugin/LobbyPlugin.java
@@ -36,12 +36,12 @@ public class LobbyPlugin extends JavaPlugin {
     chat.send((Player) Bukkit.getOnlinePlayers(), "This Plugin is currently in Maintenance!");
     plugin = this;
    // getCommand("setspawn").setExecutor((CommandExecutor)new setspawn());
-    getCommand("setLobbyWarp1").setExecutor(new SpawnSetter("setspawn", "tdrstudios.lobby.perms.setSpawn", getConfig()));
-    getCommand("setLobbyWarp2").setExecutor(new SpawnSetter("setWarp1", "tdrstudios.lobby.perms.setWarp1", getConfig()));
-    getCommand("setLobbyWarp3").setExecutor(new SpawnSetter("setWarp2", "tdrstudios.lobby.perms.setWarp2", getConfig()));
-    getCommand("setLobbyWarp4").setExecutor(new SpawnSetter("setWarp3", "tdrstudios.lobby.perms.setWarp3", getConfig()));
-    getCommand("setLobbyWarp5").setExecutor(new SpawnSetter("setWarp4", "tdrstudios.lobby.perms.setWarp4", getConfig()));
-    getCommand("setLobbyWarp6").setExecutor(new SpawnSetter("setWarp5", "tdrstudios.lobby.perms.setWarp5", getConfig()));
+    getCommand("setLobbySpawn").setExecutor(new SpawnSetter("setspawn", "tdrstudios.lobby.perms.setSpawn", getConfig()));
+    getCommand("setLobbyWarp1").setExecutor(new SpawnSetter("setWarp1", "tdrstudios.lobby.perms.setWarp1", getConfig()));
+    getCommand("setLobbyWarp2").setExecutor(new SpawnSetter("setWarp2", "tdrstudios.lobby.perms.setWarp2", getConfig()));
+    getCommand("setLobbyWarp3").setExecutor(new SpawnSetter("setWarp3", "tdrstudios.lobby.perms.setWarp3", getConfig()));
+    getCommand("setLobbyWarp4").setExecutor(new SpawnSetter("setWarp4", "tdrstudios.lobby.perms.setWarp4", getConfig()));
+    getCommand("setLobbyWarp5").setExecutor(new SpawnSetter("setWarp5", "tdrstudios.lobby.perms.setWarp5", getConfig()));
     getCommand("spawn").setExecutor((CommandExecutor)new SpawnCommand());
 
 
