c295351c1d3499f1271aed84a62269179aa7fee1
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-12-21 09:52:52 +0100

    Add Constructor and Dynamic

--- src/main/java/net/juligames/lobbyplugin/msgs/LackingPermissionMessage.java
+++ src/main/java/net/juligames/lobbyplugin/msgs/LackingPermissionMessage.java
@@ -1,4 +1,12 @@
 package net.juligames.lobbyplugin.msgs;
 
-public class LackingPermissionMessage {
+import net.juligames.lobbyplugin.Chat;
+import org.bukkit.ChatColor;
+import org.bukkit.permissions.Permission;
+
+public class LackingPermissionMessage extends Message{
+
+    public LackingPermissionMessage(Permission permission) {
+        super("tdrstudios.permission.lacking." + permission.getName() , Chat.getErrorColor() +"You are lacking the Permission : " + Chat.getAccentColor() + permission.getName() + Chat.getChatColor() + "!");
+    }
 }
