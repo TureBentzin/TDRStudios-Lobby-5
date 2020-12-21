601c9f54743174962975ba1b9e65d107e300e3e6
dseelp <46624152+DSeeLPYT@users.noreply.github.com>
2020-12-21 17:05:22 +0100

    Fixed startup errors

--- src/main/java/net/juligames/lobbyplugin/commands/SpawnSetter.java
+++ src/main/java/net/juligames/lobbyplugin/commands/SpawnSetter.java
@@ -23,10 +23,6 @@ public class SpawnSetter implements CommandExecutor{
     private Console log = new Console(getName(), this.getClass().getSimpleName(), "!");
     private Command command;
 
-    public void setCommand(Command command) {
-        this.command = command;
-    }
-
     public Command getCommand() {
         return command;
     }
@@ -58,51 +54,51 @@ public class SpawnSetter implements CommandExecutor{
         return name;
     }
 
-    public SpawnSetter(String pname , String permission, FileConfiguration configuration) {
+    public SpawnSetter(String cmdName, String pname , String permission, FileConfiguration configuration) {
         log.send("Init : " + pname);
-        registerMSGs();
+        command = LobbyPlugin.getPlugin().getCommand(cmdName);
         setConfig(configuration);
         setName(pname);
         setPermission(new Permission(permission, "The Permission for : " + getName() + "!"));
+        registerMSGs();
     }
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        setCommand(command);
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                Chat chat = new Chat();
-                chat.setPlayers(new Player[]{player});
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            Chat chat = new Chat();
+            chat.setPlayers(new Player[]{player});
 
-                if (player.hasPermission(getPermission())) {
-                    if (args.length == 0) {
-                        FileConfiguration config = getConfig();
-                        config.set(getName()+ ".World", player.getWorld().getName());
-                        config.set(getName() +".X", Double.valueOf(player.getLocation().getX()));
-                        config.set(getName() +".Y", Double.valueOf(player.getLocation().getY()));
-                        config.set(getName() +".Z", Double.valueOf(player.getLocation().getZ()));
-                        config.set(getName() +".Yaw", Float.valueOf(player.getLocation().getYaw()));
-                        config.set(getName() +".Pitch", Float.valueOf(player.getLocation().getPitch()));
-                        LobbyPlugin.getPlugin().saveConfig();
+            if (player.hasPermission(getPermission())) {
+                if (args.length == 0) {
+                    FileConfiguration config = getConfig();
+                    config.set(getName()+ ".World", player.getWorld().getName());
+                    config.set(getName() +".X", Double.valueOf(player.getLocation().getX()));
+                    config.set(getName() +".Y", Double.valueOf(player.getLocation().getY()));
+                    config.set(getName() +".Z", Double.valueOf(player.getLocation().getZ()));
+                    config.set(getName() +".Yaw", Float.valueOf(player.getLocation().getYaw()));
+                    config.set(getName() +".Pitch", Float.valueOf(player.getLocation().getPitch()));
+                    LobbyPlugin.getPlugin().saveConfig();
 
-                        chat.sendMessage("spawn.setter." + getName() + ".success");
-                    } else {
-                       // player.sendMessage("§8[§e!§8]§a Bitte benutze /setlobbyspawn");
-                       chat.sendMessage(LobbyPlugin.getMessageManager().getMessageByName("spawn.setter." + getName() +".error.arguments"));
-                       chat.sendMessage("spawn.setter." + getName() + "error.arguments");
-
-                    }
+                    chat.sendMessage("spawn.setter." + getName() + ".success");
                 } else {
-                    chat.sendMessage(LackingPermissionMessage.getNameFIX(permission));
-                }
-            }
-            return false;
-        }
-        public void  registerMSGs() {
-            MessageManager manager = LobbyPlugin.getMessageManager();
-            manager.registerMessage(new Message("spawn.setter." + getName() +".success","You have set the position for " + Chat.getAccentColor() + getName() + Chat.getChatColor() + "!"));
-            manager.registerMessage(new Message("spawn.setter." + getName() + ".error.arguments" , Chat.getErrorColor() + "Please care about the usage : " + Chat.getAccentColor() + getCommand().getUsage() + Chat.getChatColor() + "!"));
-            manager.registerMessage(new LackingPermissionMessage(permission));
+                    // player.sendMessage("§8[§e!§8]§a Bitte benutze /setlobbyspawn");
+                    chat.sendMessage(LobbyPlugin.getMessageManager().getMessageByName("spawn.setter." + getName() +".error.arguments"));
+                    chat.sendMessage("spawn.setter." + getName() + "error.arguments");
 
+                }
+            } else {
+                chat.sendMessage(LackingPermissionMessage.getNameFIX(permission));
+            }
         }
+        return false;
+    }
+    public void  registerMSGs() {
+        MessageManager manager = LobbyPlugin.getMessageManager();
+        manager.registerMessage(new Message("spawn.setter." + getName() +".success","You have set the position for " + Chat.getAccentColor() + getName() + Chat.getChatColor() + "!"));
+        manager.registerMessage(new Message("spawn.setter." + getName() + ".error.arguments" , Chat.getErrorColor() + "Please care about the usage : " + Chat.getAccentColor() + getCommand().getUsage() + Chat.getChatColor() + "!"));
+        manager.registerMessage(new LackingPermissionMessage(permission));
+
+    }
 
 
 }
