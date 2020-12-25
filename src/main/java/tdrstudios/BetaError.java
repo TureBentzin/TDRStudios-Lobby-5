d1a7a96d618f5f235c823b927de93a28607c5d9e
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-12-25 18:57:36 +0100

    Add Kick

--- src/main/java/tdrstudios/BetaError.java
+++ src/main/java/tdrstudios/BetaError.java
@@ -2,8 +2,11 @@ package tdrstudios;
 
 import de.bentzin.tools.DevTools;
 import de.bentzin.tools.time.Timing;
+import net.juligames.lobbyplugin.Chat;
+import net.juligames.lobbyplugin.LobbyPlugin;
 import net.juligames.lobbyplugin.utils.StopServerOperation;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -12,7 +15,9 @@ public class BetaError extends Error{
 
     public BetaError() {
         System.err.println("You are running a beta software without accepting the risk of doing this! So we have to stop the server!");
-
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            player.kickPlayer(Chat.getPrefix() + "\n" + Chat.getErrorColor() + "This server runs a unsupported version of " + LobbyPlugin.getPlugin().getName() + " and don't accept the risks of doing that!" + "\"" + Chat.getChatColor() +"your TDRStudios Team" ); //Beta Kick!
+        }
         DevTools.getOperationManager().executeOperation(new StopServerOperation());
     }
 
