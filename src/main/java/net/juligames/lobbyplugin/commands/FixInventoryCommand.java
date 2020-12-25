d13560ce52e71b7d978be49b5e5e6e74b516c4de
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-12-25 13:20:02 +0100

    Update FixInventoryCommand.java

--- src/main/java/net/juligames/lobbyplugin/commands/FixInventoryCommand.java
+++ src/main/java/net/juligames/lobbyplugin/commands/FixInventoryCommand.java
@@ -4,9 +4,12 @@ import net.juligames.lobbyplugin.LobbyPlugin;
 import net.juligames.lobbyplugin.msgs.LackingPermissionMessage;
 import net.juligames.lobbyplugin.msgs.MessageManager;
 import net.juligames.lobbyplugin.msgs.UsageMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 
 public class FixInventoryCommand implements CommandExecutor {
@@ -36,6 +39,25 @@ public class FixInventoryCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        if(sender instanceof Player) {
+
+        }else {
+            if(sender instanceof ConsoleCommandSender) {
+                if(args.length == 1) {
+                    if(Bukkit.getPlayer(args[0]) != null) {
+
+                    }else {
+                     if(args[0] == "all" | args[0] == "*" | args[0] == "@a") {
+
+                     }
+                    }
+                }else {
+                    sender.sendMessage("Please care about the usage: /fixinventory [Player/'*']");
+                }
+            }else {
+                sender.sendMessage("Error: You cant execute this! -- this is a Problem of your sendertype! Report this error to the Devs if you aren´t using an CommandBlock!");
+            }
+        }
         return true;
     }
 
