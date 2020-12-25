51c5387b72036012605f2807942cd1762d27391a
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2020-12-25 21:19:52 +0100

    Update JoinListener.java

--- src/main/java/net/juligames/lobbyplugin/listeners/JoinListener.java
+++ src/main/java/net/juligames/lobbyplugin/listeners/JoinListener.java
@@ -7,17 +7,12 @@ import net.juligames.lobbyplugin.utils.inventory.InventoryUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
-import sun.security.krb5.Config;
 import tdrstudios.Work_In_Progress;
 
 import java.text.SimpleDateFormat;
@@ -25,37 +20,37 @@ import java.util.Date;
 
 @Work_In_Progress
 public class JoinListener implements Listener {
-  @EventHandler
+    @EventHandler
 /**
  * @param PlayerJoinEvent event
  * @see PlayerJoinEvent
  */
-  public void onPlayerJoin(PlayerJoinEvent event) {
-    event.setJoinMessage( ConfigUtils.getConfig().getString("tdrstudios.join.msg").replace("%Player%" , event.getPlayer().getDisplayName()));
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(ConfigUtils.getConfig().getString("tdrstudios.join.msg").replace("%Player%", event.getPlayer().getDisplayName()));
 
-    Player player = event.getPlayer();
-    Chat chat = new Chat();
-    chat.setPlayers(new Player[]{player});
-    SimpleDateFormat formatter = new SimpleDateFormat(ConfigUtils.getConfig().getString("tdrstudios.join.date.pattern"));
-    SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy");
-    Date date = new Date();
-    System.out.println("Current Server Time: " + formatter.format(date));
-    chat.send(ConfigUtils.getConfig().getString("tdrstudios.join.welcome").replace("%Player%" , player.getName()).replace("%Date%" , formatter.format(date)));
+        Player player = event.getPlayer();
+        Chat chat = new Chat();
+        chat.setPlayers(new Player[]{player});
+        SimpleDateFormat formatter = new SimpleDateFormat(ConfigUtils.getConfig().getString("tdrstudios.join.date.pattern"));
+        SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy");
+        Date date = new Date();
+        System.out.println("Current Server Time: " + formatter.format(date));
+        chat.send(ConfigUtils.getConfig().getString("tdrstudios.join.welcome").replace("%Player%", player.getName()).replace("%Date%", formatter.format(date)));
 
-    InventoryUtils.setInventory(player);
+        InventoryUtils.setInventory(player);
 
-    FileConfiguration config = LobbyPlugin.getPlugin().getConfig();
-    World world = Bukkit.getWorld(config.getString("Spawn.World"));
-    double x = config.getDouble("Spawn.X");
-    double y = config.getDouble("Spawn.Y");
-    double z = config.getDouble("Spawn.Z");
-    float yaw = (float)config.getDouble("Spawn.Yaw");
-    float pitch = (float)config.getDouble("Spawn.Pitch");
-    Location location = new Location(world, x, y, z, yaw, pitch);
-    player.teleport(location);
-    player.setGameMode(GameMode.valueOf(ConfigUtils.getConfig().getString("tdrstudios.defaultGameMode"))); // DEBUG @WIP
+        FileConfiguration config = LobbyPlugin.getPlugin().getConfig();
+        World world = Bukkit.getWorld(config.getString("Spawn.World"));
+        double x = config.getDouble("Spawn.X");
+        double y = config.getDouble("Spawn.Y");
+        double z = config.getDouble("Spawn.Z");
+        float yaw = (float) config.getDouble("Spawn.Yaw");
+        float pitch = (float) config.getDouble("Spawn.Pitch");
+        Location location = new Location(world, x, y, z, yaw, pitch);
+        player.teleport(location);
+        player.setGameMode(GameMode.valueOf(ConfigUtils.getConfig().getString("tdrstudios.defaultGameMode"))); // DEBUG @WIP
 
-    player.setExp(1.0F);
-    player.setLevel(Integer.parseInt(formatter1.format(date)));
-  }
+        player.setExp(ConfigUtils.getConfig().getInt("tdrstudios.hotbar.xp"));
+        player.setLevel(Integer.parseInt(formatter1.format(date)));
+    }
 }
