85df0f7b784dcd323cf783352f996a81978e12a2
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-12-26 10:44:08 +0100

    Update ConfigUtils.java

--- src/main/java/net/juligames/lobbyplugin/utils/config/ConfigUtils.java
+++ src/main/java/net/juligames/lobbyplugin/utils/config/ConfigUtils.java
@@ -18,7 +18,7 @@ public class ConfigUtils {
     }
     public static void registerAllConfigurations() {
         registerConfiguration("beta.acceptRisk" , false);
-        registerConfiguration("bata.enablebetabook" , true);
+        registerConfiguration("bata.enableBetaBook" , true);
 
         registerConfiguration("tdrstudios.join.msg" , "§8[§e+§8]§a %Player%");
         registerConfiguration("tdrstudios.leave.msg");
