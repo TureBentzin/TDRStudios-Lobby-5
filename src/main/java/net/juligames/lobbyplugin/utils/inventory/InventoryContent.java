385c17752ecd4ecc7571a57290b03547cd7925b0
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2020-12-26 08:43:00 +0100

    Create InventoryContent.java

---
+++ src/main/java/net/juligames/lobbyplugin/utils/inventory/InventoryContent.java
@@ -0,0 +1,81 @@
+package net.juligames.lobbyplugin.utils.inventory;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class InventoryContent {
+
+    public InventoryContent(Material material, String displayName, int count, boolean isEnchant , int slot) {
+        setCount(count);
+        setMaterial(material);
+        setItemStack(new ItemStack(getMaterial(), getCount()));
+
+        if(isEnchant) {
+            getMeta().addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL , 1 , true);
+        }
+        getMeta().setDisplayName(displayName);
+
+        getItemStack().setItemMeta(getMeta());
+        setSlot(slot);
+    }
+    private int count;
+    private ItemStack itemStack;
+    private Material material;
+    private ItemMeta meta;
+
+    private int slot;
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+
+    public void setMeta(ItemMeta meta) {
+        this.meta = meta;
+    }
+
+    public ItemMeta getMeta() {
+        return meta;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    private ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryContent{" +
+                "count=" + count +
+                ", itemStack=" + itemStack +
+                ", material=" + material +
+                ", meta=" + meta +
+                '}';
+    }
+
+    public ItemStack toItemStack() { return getItemStack();}
+}
