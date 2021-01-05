c85b8f8951f608c62b590441803c05976e400497
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-01-05 09:50:30 +0100

    Add Link to Git

--- src/main/java/de/tdrstudios/lobbyplugin/commands/VersionCheckCommand.java
+++ src/main/java/de/tdrstudios/lobbyplugin/commands/VersionCheckCommand.java
@@ -3,6 +3,9 @@ package de.tdrstudios.lobbyplugin.commands;
 import de.tdrstudios.lobbyplugin.Chat;
 import de.tdrstudios.lobbyplugin.LobbyPlugin;
 import de.tdrstudios.lobbyplugin.msgs.UsageMessage;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -33,10 +36,16 @@ public class VersionCheckCommand implements CommandExecutor {
       Player p = (Player) sender;
       chat.setPlayers(new Player[]{p});
       if (args.length == 0) {
+
+        BaseComponent baseComponent = new TextComponent();
+        baseComponent.addExtra("Check the process of development on GitHub!");
+        ClickEvent clickEvent = baseComponent.getClickEvent();
+        baseComponent.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL , "https://github.com/TDRMinecraft/ZZockers_Lobby_Edit"));
         //p.sendMessage("§3§lV1.2 Vom 18.09.2020 §4§l Programmiert von " +LobbyPlugin.getPlugin().getDescription().getAuthors());
         chat.sendMessage("Plugin: " + LobbyPlugin.getPlugin().getDescription().getVersion());
         chat.sendMessage("Version: " + LobbyPlugin.getPlugin().getDescription().getVersion());
         chat.sendMessage("By: " + LobbyPlugin.getPlugin().getDescription().getAuthors());
+        p.spigot().sendMessage(baseComponent);
 
       } else {
         chat.sendMessage(UsageMessage.getNameFIX(getCommand()));
