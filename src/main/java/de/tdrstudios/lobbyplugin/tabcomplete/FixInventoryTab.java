d1a3895c01d580cedbd11a374af2ba030aeda732
TDR_Minecraft <tdr.minecraft@gmail.com>
2021-01-10 13:16:30 +0100

    Update FixInventoryTab.java

--- src/main/java/de/tdrstudios/lobbyplugin/tabcomplete/FixInventoryTab.java
+++ src/main/java/de/tdrstudios/lobbyplugin/tabcomplete/FixInventoryTab.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Deprecated
 public class FixInventoryTab implements TabCompleter {
 
     private Permission permission;
