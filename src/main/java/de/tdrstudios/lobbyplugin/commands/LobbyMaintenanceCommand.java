ba447a92b2f3577aa8a5b2fb2ccef0bce38ebcd9
TDR_Minecraft <tdr.minecraft@gmail.com>
2021-01-12 18:42:48 +0100

    Update LobbyMaintenanceCommand.java

--- src/main/java/de/tdrstudios/lobbyplugin/commands/LobbyMaintenanceCommand.java
+++ src/main/java/de/tdrstudios/lobbyplugin/commands/LobbyMaintenanceCommand.java
@@ -1,4 +1,35 @@
 package de.tdrstudios.lobbyplugin.commands;
 
-public class LobbyMaintenanceCommand {
+import de.tdrstudios.lobbyplugin.tabcomplete.Argument;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class LobbyMaintenanceCommand extends MyCommand {
+
+
+    // STOPSHIP: 12.01.2021
+    /**
+     * @param command
+     * @param permission
+     * @param arguments
+     * @STOPSHIP: 12.01.2021
+     * @Autonom This is for maintenance not for other weird stuff! Thanks!
+     */
+    public LobbyMaintenanceCommand(Command command, Permission permission, List<Argument>[] arguments) {
+        super(command, permission, arguments);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(CommandSender commandSender, Command command, String label, String[] args) {
+        return null;
+    }
 }
