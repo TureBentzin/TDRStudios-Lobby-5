6aa6254fa840760f49229a0dec6d823d382f6aac
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-01-13 09:40:25 +0100

    Update SpawnCommand.java

--- src/main/java/de/tdrstudios/lobbyplugin/commands/SpawnCommand.java
+++ src/main/java/de/tdrstudios/lobbyplugin/commands/SpawnCommand.java
@@ -1,8 +1,12 @@
 package de.tdrstudios.lobbyplugin.commands;
 
+import de.tdrstudios.additional.Work_In_Progress;
+import de.tdrstudios.lobbyplugin.Chat;
 import de.tdrstudios.lobbyplugin.LobbyPlugin;
+import de.tdrstudios.lobbyplugin.msgs.LackingPermissionMessage;
 import de.tdrstudios.lobbyplugin.msgs.Message;
 import de.tdrstudios.lobbyplugin.msgs.MessageManager;
+import de.tdrstudios.lobbyplugin.msgs.UsageMessage;
 import de.tdrstudios.lobbyplugin.tabcomplete.Argument;
 import de.tdrstudios.lobbyplugin.utils.config.ConfigUtils;
 import org.bukkit.Bukkit;
@@ -23,7 +27,6 @@ public class SpawnCommand extends MyCommand {
   /**
    * @param command
    * @param permission
-   * @param arguments
    * @STOPSHIP: 10.01.2021
    * @Autonom This can manage your TabComplete and other stuff fully self!
    */
@@ -33,22 +36,29 @@ public class SpawnCommand extends MyCommand {
     registerTabComplete();
   }
 
+  @Work_In_Progress
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player) {
       Player player = (Player)sender;
+      Chat chat = new Chat();
+      chat.setPlayers(new Player[]{player});
+      FileConfiguration config = LobbyPlugin.getPlugin().getConfig();
       if (args.length == 0) {
-        FileConfiguration config = LobbyPlugin.getPlugin().getConfig();
-        World world = Bukkit.getWorld(config.getString("Spawn.World"));
-        double x = config.getDouble("Spawn.X");
-        double y = config.getDouble("Spawn.Y");
-        double z = config.getDouble("Spawn.Z");
-        float yaw = (float)config.getDouble("Spawn.Yaw");
-        float pitch = (float)config.getDouble("Spawn.Pitch");
-        Location location = new Location(world, x, y, z, yaw, pitch);
-        player.teleport(location);
-        player.sendMessage("§8[§eInfo§8]§a Du wurdest zum Spawn teleportiert!");
+        if(player.hasPermission(getPermission())) {
+
+          World world = Bukkit.getWorld(config.getString("Spawn.World"));
+          double x = config.getDouble("Spawn.X");
+          double y = config.getDouble("Spawn.Y");
+          double z = config.getDouble("Spawn.Z");
+          float yaw = (float) config.getDouble("Spawn.Yaw");
+          float pitch = (float) config.getDouble("Spawn.Pitch");
+          Location location = new Location(world, x, y, z, yaw, pitch);
+          player.teleport(location);
+          chat.sendMessage("tdrstudios.spawn.success");
+        }else
+          chat.sendMessage(new LackingPermissionMessage(getPermission()));
       } else {
-        player.sendMessage("§8[§e!§8]§a Bitte benutze /spawn");
+        chat.sendMessage(UsageMessage.getNameFIX(getCommand()));
       } 
     } 
     return false;
@@ -68,6 +78,7 @@ public class SpawnCommand extends MyCommand {
     messageManager.registerMessage(new Message("tdrstudios.spawn.success", ConfigUtils.getString("tdrstudios.spawn.success")));
     messageManager.registerMessage(new Message("tdrstudios.spawn.success.other", ConfigUtils.getString("tdrstudios.spawn.success.other")));
     messageManager.registerMessage(new Message("tdrstudios.spawn.success.otherB", ConfigUtils.getString("tdrstudios.spawn.success.otherB")));
+    messageManager.registerMessage(new UsageMessage(getCommand()));
 
   }
 
@@ -79,7 +90,14 @@ public class SpawnCommand extends MyCommand {
 
   @Override
   public List<String>[] registerTabComplete() {
+    List<Argument>[] r = new List[1];
     List<Argument> list1 = new ArrayList<>();
+    Argument playerArgument = new Argument("%Players%");
+    playerArgument.setPermission(new Permission(getPermission().getName() + ".other"));
+    list1.add(new Argument("*"));
+    list1.add(new Argument("@a"));
+    list1.add(new Argument("all"));
+    list1.add(playerArgument);
     return new List[0];
   }
 }
