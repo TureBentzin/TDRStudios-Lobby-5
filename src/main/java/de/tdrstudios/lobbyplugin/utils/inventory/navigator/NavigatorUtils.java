39ef9ebfbd797e4c9738b404a08491b0d673d0bc
TDR_Minecraft <tdr.minecraft@gmail.com>
2021-01-17 14:41:37 +0100

    Add Implements for NavigatorUtils.java

--- src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/navigator/NavigatorUtils.java
+++ src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/navigator/NavigatorUtils.java
@@ -1,4 +1,14 @@
 package de.tdrstudios.lobbyplugin.utils.inventory.navigator;
 
-public class NavigatorUtils {
+import de.tdrstudios.lobbyplugin.utils.inventory.InventoryContent;
+import de.tdrstudios.lobbyplugin.utils.inventory.InventoryUtilsInterface;
+
+public class NavigatorUtils  implements InventoryUtilsInterface {
+    /**
+     * @param inventoryContent Register a InventoryContent
+     */
+    @Override
+    public void registerInventoryContent(InventoryContent inventoryContent) {
+        
+    }
 }
