1b9a6e8eb144c5d3a8e9f8110d5818358c6237c0
TDR_Minecraft <tdr.minecraft@gmail.com>
2021-01-17 14:40:30 +0100

    Create InventoryUtilsInterface.java

---
+++ src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/InventoryUtilsInterface.java
@@ -0,0 +1,10 @@
+package de.tdrstudios.lobbyplugin.utils.inventory;
+
+public interface InventoryUtilsInterface {
+    /**
+     *
+     * @param inventoryContent
+     * Register a InventoryContent
+     */
+    public void registerInventoryContent(InventoryContent inventoryContent);
+}
