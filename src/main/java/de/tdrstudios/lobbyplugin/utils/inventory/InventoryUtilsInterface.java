79333d51b8af8b18fe21a384b3961ab450850900
TDR_Minecraft <tdr.minecraft@gmail.com>
2021-01-17 15:08:32 +0100

    Update InventoryUtilsInterface.java

--- src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/InventoryUtilsInterface.java
+++ src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/InventoryUtilsInterface.java
@@ -1,11 +1,45 @@
 package de.tdrstudios.lobbyplugin.utils.inventory;
+import org.bukkit.configuration.file.FileConfiguration;
+
 import java.util.*;
-public abstract interface InventoryUtilsInterface {
-    List<InventoryContent> index = new ArrayList<>();
+public abstract class InventoryUtilsInterface {
+    private static FileConfiguration c;
+
+    private static List<InventoryContent> index = new ArrayList<>();
+
+    public static List<InventoryContent> getIndex() {
+        return index;
+    }
+
+    public static void setIndex(List<InventoryContent> index) {
+        InventoryUtilsInterface.index = index;
+    }
 
     /**
      * @param inventoryContent Register a InventoryContent
      */
+    public static boolean registerInventoryContent(InventoryContent inventoryContent) {
+        if(!index.contains(inventoryContent)) {
+            index.add(inventoryContent);
+            return true;
+        }else
+        return false;
+    }
 
+    public static boolean removeInventoryContent(InventoryContent inventoryContent) {
+        if(index.contains(inventoryContent)) {
+            index.remove(inventoryContent);
+            return true;
+        }else
+        return false;
+    }
+
+    /**
+     * Gets config.
+     * @return the config
+     */
+    public static FileConfiguration getConfig() {
+        return c;
+    }
 
 }
