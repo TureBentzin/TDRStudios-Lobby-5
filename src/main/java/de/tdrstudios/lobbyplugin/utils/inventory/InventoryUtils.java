bd71426e7f90ba563f687def75827d0cca37ab0c
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-01-29 17:48:28 +0100

    remove LEGACY_setInventory

--- src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/InventoryUtils.java
+++ src/main/java/de/tdrstudios/lobbyplugin/utils/inventory/InventoryUtils.java
@@ -49,46 +49,7 @@ public class InventoryUtils {
     public static void registerInventoryContent(InventoryContent inventoryContent){
         index.add(inventoryContent);
     }
-
-    /**
-     * Legacy: set inventory.
-     *
-     * @param inventory the Player Inventory that will be set to LobbyInventory
-     * @deprecated
-     * @implNote This Method is an "relict" and cant be used for the plugin!!!!
-     */
-    @Deprecated
-
-    public static void LEGACY_setInventory(PlayerInventory inventory) {
-        inventory.clear(); // Clear all Items out of the Inventory
-
-        //List all ItemSacks
-        ItemStack item1 = new ItemStack(Material.getMaterial(ConfigUtils.getConfig().getString("tdrstudios.hotbar.nav.material")));
-        ItemMeta itemMeta = item1.getItemMeta();
-        itemMeta.setDisplayName(ConfigUtils.getConfig().getString("tdrstudios.hotbar.nav.displayName"));
-        item1.setItemMeta(itemMeta);
-        ItemStack item2 = new ItemStack(Material.getMaterial(c.getString("tdrstudios.hotbar.info.material")));
-        ItemMeta itemMeta2 = item2.getItemMeta();
-        itemMeta2.setDisplayName(ConfigUtils.getConfig().getString("tdrstudios.hotbar.info.displayName"));
-        item2.setItemMeta(itemMeta2);
-        ItemStack item3 = new ItemStack(Material.getMaterial(c.getString("tdrstudios.hotbar.settings.material")));
-        ItemMeta itemMeta3 = item3.getItemMeta();
-        itemMeta3.setDisplayName(ConfigUtils.getConfig().getString("tdrstudios.hotbar.settings.displayName"));
-        item3.setItemMeta(itemMeta3);
-        ItemStack item4 = new ItemStack(Material.getMaterial(c.getString("tdrstudios.hotbar.stick.material")));
-        ItemMeta itemMeta4 = item4.getItemMeta();
-        itemMeta4.setDisplayName(ConfigUtils.getConfig().getString("tdrstudios.hotbar.stick.displayName"));
-        item4.setItemMeta(itemMeta4);
-
-        //Set the ItemsStacks into the inventory
-        inventory.setItem(c.getInt("tdrstudios.hotbar.nav.slot"), item1);
-        inventory.setItem(c.getInt("tdrstudios.hotbar.info.slot"), item2);
-        inventory.setItem(c.getInt("tdrstudios.hotbar.settings.slot"), item3);
-        inventory.setItem(c.getInt("tdrstudios.hotbar.stick.slot"), item4);
-
-    }
-
-
+    
     /**
      * Sets inventory to lobbystandart.
      *
