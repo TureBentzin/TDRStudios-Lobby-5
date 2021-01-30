a6d1bb9f19ba7f845e4ce675b4c81277699fdc5e
TDR_Minecraft <tdr.minecraft@gmail.com>
2021-01-30 18:50:03 +0100

    Stash

--- src/main/java/de/tdrstudios/lobbyplugin/commands/SpawnSetter.java
+++ src/main/java/de/tdrstudios/lobbyplugin/commands/SpawnSetter.java
@@ -8,6 +8,7 @@ import de.tdrstudios.lobbyplugin.msgs.Message;
 import de.tdrstudios.lobbyplugin.msgs.MessageManager;
 import de.tdrstudios.lobbyplugin.msgs.UsageMessage;
 import de.tdrstudios.lobbyplugin.tabcomplete.Argument;
+import de.tdrstudios.lobbyplugin.tabcomplete.TabComplete;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -59,8 +60,7 @@ public class SpawnSetter extends MyCommand{
     }
 
     public SpawnSetter(String cmdName, String pname , String permission, FileConfiguration configuration) {
-
-        super(LobbyPlugin.getPlugin().getCommand(pname) , new Permission(permission) , new ArrayList<Argument>[]{});
+        super(LobbyPlugin.getPlugin().getCommand(pname) , new Permission(permission) , TabComplete.getNullList());
         log.send("Init : " + pname);
         command = LobbyPlugin.getPlugin().getCommand(cmdName);
         setConfig(configuration);
