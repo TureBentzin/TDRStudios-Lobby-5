7b551fb13eecdb36e477bac7cbcc3cfc8f9f74a1
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-03-10 09:16:42 +0100

    Create DebugConsole.java

---
+++ src/main/java/de/tdrstudios/additional/debug/DebugConsole.java
@@ -0,0 +1,12 @@
+package de.tdrstudios.additional.debug;
+
+import de.bentzin.tools.console.Console;
+
+import java.lang.reflect.Method;
+
+public class DebugConsole extends Console {
+    public DebugConsole(Class clazz) {
+        super(clazz.getName(),"DEBUG-" + clazz.getSimpleName(), ";");
+    }
+    
+}
