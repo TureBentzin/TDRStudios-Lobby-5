dcd566346748f1df5d9f69f62c083ca3f63fbe30
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-03-10 09:18:09 +0100

    Update DebugConsole.java

--- src/main/java/de/tdrstudios/additional/debug/DebugConsole.java
+++ src/main/java/de/tdrstudios/additional/debug/DebugConsole.java
@@ -8,5 +8,12 @@ public class DebugConsole extends Console {
     public DebugConsole(Class clazz) {
         super(clazz.getName(),"DEBUG-" + clazz.getSimpleName(), ";");
     }
-    
+
+    /**
+     *
+     * @param key - for global debug
+     */
+    public DebugConsole(String key) {
+        super(key, key, "");
+    }
 }
