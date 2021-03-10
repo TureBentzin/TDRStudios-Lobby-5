3048cfa58b7c65db226e516b04be3c566b2a2589
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-03-10 09:26:15 +0100

    add active

--- src/main/java/de/tdrstudios/additional/debug/DebugConsole.java
+++ src/main/java/de/tdrstudios/additional/debug/DebugConsole.java
@@ -3,6 +3,17 @@ package de.tdrstudios.additional.debug;
 import de.bentzin.tools.console.Console;
 
 public class DebugConsole extends Console {
+
+    protected boolean active = false;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
     public DebugConsole(Class clazz) {
         super(clazz.getName(),"DEBUG-" + clazz.getSimpleName(), ";");
     }
@@ -20,4 +31,18 @@ public class DebugConsole extends Console {
     public static DebugConsole getDebugConsole() {
         return debugConsole;
     }
+
+
+    @Override
+    public void send(String msg) {
+        if(active)
+        super.send(msg);
+    }
+
+    @Override
+    public void send(String msg, String pPrefix, String pSuffix) {
+        if(active)
+        super.send(msg, pPrefix, pSuffix);
+    }
+
 }
