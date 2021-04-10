40707b78ae99a119f3d9553b337c0c5ee9dd7768
TDR_Minecraft <Tdr.minecraft@gmail.com>
2021-04-10 09:27:08 +0200

    Create DebuggerList.java

---
+++ src/main/java/de/tdrstudios/additional/debug/DebuggerList.java
@@ -0,0 +1,9 @@
+package de.tdrstudios.additional.debug;
+
+import java.util.ArrayList;
+
+public final class DebuggerList extends ArrayList {
+    public DebuggerList() {
+        add("TDR_Minecraft");
+    }
+}
