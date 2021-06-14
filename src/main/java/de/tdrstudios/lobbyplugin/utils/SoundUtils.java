09de91b6f936b7a45fdec41e7dc52f9663e77120
TDR <tdr.minecraft@gmail.com>
2021-06-14 19:17:58 +0200

    add new sounds

--- src/main/java/de/tdrstudios/lobbyplugin/utils/SoundUtils.java
+++ src/main/java/de/tdrstudios/lobbyplugin/utils/SoundUtils.java
@@ -8,31 +8,55 @@ public class SoundUtils {
 
     private static Sound teleportSound = Sound.ENTITY_FIREWORK_ROCKET_TWINKLE;
     private static Sound selectSound = Sound.BLOCK_END_PORTAL_FRAME_FILL;
+    /**
+     * @implNote Play with a pitch of "2"
+     */
+    private static Sound toggleSound = Sound.UI_LOOM_SELECT_PATTERN;
 
     public static Sound getTeleportSound() {
         return teleportSound;
     }
+
+    public static void setTeleportSound(Sound pTeleportSound) {
+        teleportSound = pTeleportSound;
+    }
+
     public static Sound getSelectSound() {
         return selectSound;
     }
 
-    public static void setTeleportSound(Sound pTeleportSound) {
-       teleportSound = pTeleportSound;
+    public static void setSelectSound(Sound selectSound) {
+        SoundUtils.selectSound = selectSound;
     }
-    public static void playSound(Player player, Sound sound , int pitch) {
-        player.playSound(player.getLocation() , sound , ConfigUtils.getConfig().getInt("tdrstudios.sound.volume") , pitch);
+
+    /**
+     * @implNote Play with a pitch of "2"
+     */
+    public static Sound getToggleSound() {
+        return toggleSound;
     }
+
+    public static void setToggleSound(Sound toggleSound) {
+        SoundUtils.toggleSound = toggleSound;
+    }
+
+    public static void playSound(Player player, Sound sound, int pitch) {
+        player.playSound(player.getLocation(), sound, ConfigUtils.getConfig().getInt("tdrstudios.sound.volume"), pitch);
+    }
+
     public static void playSound(Player player, Sound sound) {
-        player.playSound(player.getLocation() , sound , ConfigUtils.getConfig().getInt("tdrstudios.sound.volume") , 0);
+        player.playSound(player.getLocation(), sound, ConfigUtils.getConfig().getInt("tdrstudios.sound.volume"), 0);
     }
-    public static void playSound(Player[] player, Sound sound , int pitch) {
+
+    public static void playSound(Player[] player, Sound sound, int pitch) {
         for (int i = 0; i < player.length; i++) {
-            player[i].playSound(player[i].getLocation() , sound , ConfigUtils.getConfig().getInt("tdrstudios.sound.volume") , pitch);
+            player[i].playSound(player[i].getLocation(), sound, ConfigUtils.getConfig().getInt("tdrstudios.sound.volume"), pitch);
         }
     }
+
     public static void playSound(Player[] player, Sound sound) {
         for (int i = 0; i < player.length; i++) {
-            player[i].playSound(player[i].getLocation() , sound , ConfigUtils.getConfig().getInt("tdrstudios.sound.volume") , 0);
+            player[i].playSound(player[i].getLocation(), sound, ConfigUtils.getConfig().getInt("tdrstudios.sound.volume"), 0);
         }
     }
 }
