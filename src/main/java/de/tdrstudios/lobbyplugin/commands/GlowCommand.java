a96f5912e6bd437bea8fecf5258f88f71e0bc622
TDR <tdr.minecraft@gmail.com>
2021-06-15 17:56:48 +0200

    completed "GlowCommand"
    /glow [Player/on/off] <on/off>

--- src/main/java/de/tdrstudios/lobbyplugin/commands/GlowCommand.java
+++ src/main/java/de/tdrstudios/lobbyplugin/commands/GlowCommand.java
@@ -80,47 +80,72 @@ public class GlowCommand extends SimpleCommand {
                         setGlow(player, false);
                     return;
                 }
+                else if(Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))) {
+                    if(player.hasPermission(getOtherPermission())) {
+                        toggleGlow(Bukkit.getPlayer(args[0]), sender);
+                    }else {
+                        chat.sendFast(player, new LackingPermissionMessage(getOtherPermission()));
+                    }
+                }
 
-            }
+            }else
+            if(Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))) {
+                if(sender.hasPermission(getOtherPermission())) {
+                    toggleGlow(Bukkit.getPlayer(args[0]), sender);
+                }else {
+                    chat.sendFast(sender, new LackingPermissionMessage(getOtherPermission()));
+                }
+            }else
             printUsageMessage();
+            return;
         } if (args.length < 3) {
+            if (sender.hasPermission(getOtherPermission())) {
+                if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))) {
 
-            if (Bukkit.getOnlinePlayers().contains(args[0])) {
-
-                Player target = Bukkit.getPlayer(args[0]);
-                if (sender.hasPermission(getOtherPermission())) {
-                    if(target.equals(sender))
-                        printUsageMessage();
-                    if(args.length == 1)
-                        toggleGlow(target, sender);
-                    else{
-                        if(args[1].equalsIgnoreCase("on")) {
-                            if (isGlowing(target))
-                                chat.send(Chat.getErrorColor() + ConfigUtils.getString("tdrstudios.commands.glow.msgs.fail.already").replace("%Status%", Chat.getAccentColor() + "on" + Chat.getErrorColor()).replace("%Player%", Chat.getAccentColor() + target.getName() + Chat.getErrorColor()));
-                            else
-                                setGlow(target, true);
-
-                        }else if(args[1].equalsIgnoreCase("off")) {
-                            if (isGlowing(target))
-                                chat.send(Chat.getErrorColor() + ConfigUtils.getString("tdrstudios.commands.glow.msgs.fail.already").replace("%Status%", Chat.getAccentColor() + "off" + Chat.getErrorColor()).replace("%Player%", Chat.getAccentColor() + target.getName() + Chat.getErrorColor()));
-                            else
-                                setGlow(target, false);
-
-                        }else{
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (sender.hasPermission(getOtherPermission())) {
+                        if (target.equals(sender)) {
                             printUsageMessage();
+                            return;
                         }
+                        if (args.length == 1)
+                            toggleGlow(target, sender);
+                        else {
+                            if (args[1].equalsIgnoreCase("on")) {
+                                if (isGlowing(target))
+                                    chat.sendFast(sender, Chat.getErrorColor() + ConfigUtils.getString("tdrstudios.commands.glow.msgs.fail.already").replace("%Status%", Chat.getAccentColor() + "on" + Chat.getErrorColor()).replace("%Player%", Chat.getAccentColor() + target.getName() + Chat.getErrorColor()));
+                                else {
+                                    setGlow(target, true, sender);
+                                    chat.sendFast(sender, ConfigUtils.getString("tdrstudios.commands.glow.msgs.toggled-byMe").replace("%Player%", Chat.getAccentColor() + target.getName() + Chat.getChatColor()).replace("%Status%", Chat.getAccentColor() + "on" + Chat.getChatColor()));
+                                }
+
+                            } else if (args[1].equalsIgnoreCase("off")) {
+                                if (isGlowing(target))
+                                    chat.sendFast(sender, Chat.getErrorColor() + ConfigUtils.getString("tdrstudios.commands.glow.msgs.fail.already").replace("%Status%", Chat.getAccentColor() + "off" + Chat.getErrorColor()).replace("%Player%", Chat.getAccentColor() + target.getName() + Chat.getErrorColor()));
+                                else {
+                                    setGlow(target, false, sender);
+                                    chat.sendFast(sender, ConfigUtils.getString("tdrstudios.commands.glow.msgs.toggled-byMe").replace("%Player%", Chat.getAccentColor() + target.getName() + Chat.getChatColor()).replace("%Status%", Chat.getAccentColor() + "off" + Chat.getChatColor()));
+                                }
+                            } else {
+                                printUsageMessage();
+                            }
+                        }
+
+                    } else {
+                        Chat.sendFast(sender, new LackingPermissionMessage(getOtherPermission()));
                     }
 
                 } else {
-                    Chat.sendFast(sender, new LackingPermissionMessage(getOtherPermission()));
+                    if (args[0].equalsIgnoreCase("on") || args[0].equalsIgnoreCase("off")) {
+                        Chat.sendFast(sender, new OnlySenderMessage(SenderType.PLAYER));
+                    } else {
+                        chat.sendFast(sender, Chat.getErrorColor() + "The Player " + Chat.getAccentColor() + args[0] + Chat.getErrorColor() + " is not online!");
+                    }
                 }
-
-            } else {
-                if (args[0].equalsIgnoreCase("on") || args[0].equalsIgnoreCase("off")) {
-                    Chat.sendFast(sender, new OnlySenderMessage(SenderType.PLAYER));
-                } else {
-                    chat.send(Chat.getErrorColor() + "The Player " + Chat.getAccentColor() + args[0] + Chat.getErrorColor() + " is not online!");
-                }
+            }
+            else {
+              chat.sendFast(sender, new LackingPermissionMessage(getOtherPermission()));
+              return;
             }
 
 
@@ -166,9 +191,13 @@ public class GlowCommand extends SimpleCommand {
         if (isGlowing(player)) {
             player.removePotionEffect(GLOW_EFFECT.getType());
             getPlayerChat().send(player, getToggledMessage(false, initiator));
+            chat.sendFast(initiator, ConfigUtils.getString("tdrstudios.commands.glow.msgs.toggled-byMe").replace("%Player%", Chat.getAccentColor() + player.getName() + Chat.getChatColor()).replace("%Status%", Chat.getAccentColor() + "off" + Chat.getChatColor()));
+
         } else {
             GLOW_EFFECT.apply(player);
             getPlayerChat().send(player, getToggledMessage(true, initiator));
+            chat.sendFast(initiator, ConfigUtils.getString("tdrstudios.commands.glow.msgs.toggled-byMe").replace("%Player%", Chat.getAccentColor() + player.getName() + Chat.getChatColor()).replace("%Status%", Chat.getAccentColor() + "on" + Chat.getChatColor()));
+
         }
         SoundUtils.playSound(player, TOGGLE_SOUND, 2);
     }
@@ -176,9 +205,13 @@ public class GlowCommand extends SimpleCommand {
         if (!b) {
             player.removePotionEffect(GLOW_EFFECT.getType());
             getPlayerChat().send(player, getToggledMessage(false, initiator));
+            chat.sendFast(initiator, ConfigUtils.getString("tdrstudios.commands.glow.msgs.toggled-byMe").replace("%Player%", Chat.getAccentColor() + player.getName() + Chat.getChatColor()).replace("%Status%", Chat.getAccentColor() + "on" + Chat.getChatColor()));
+
         } else {
             GLOW_EFFECT.apply(player);
             getPlayerChat().send(player, getToggledMessage(true, initiator));
+            chat.sendFast(initiator, ConfigUtils.getString("tdrstudios.commands.glow.msgs.toggled-byMe").replace("%Player%", Chat.getAccentColor() + player.getName() + Chat.getChatColor()).replace("%Status%", Chat.getAccentColor() + "off" + Chat.getChatColor()));
+
         }
         SoundUtils.playSound(player, TOGGLE_SOUND, 2);
     }
